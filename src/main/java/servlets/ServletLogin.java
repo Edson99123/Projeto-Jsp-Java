141f1c6539c8192caa9ecedb67f4837e83767d92
Edson Sales <edsonsales99@gmail.com>
2021-11-22 23:39:18 -0500

    Criação da estrutura de pacotes e pastas do projeto

--- src/main/java/servlets/ServletLogin.java
+++ src/main/java/servlets/ServletLogin.java
@@ -1,38 +1,31 @@
 package servlets;
 
 import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class ServletLogin
- */
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/ServletLogin")/*Mapeamento URL*/
 public class ServletLogin extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
+	
     public ServletLogin() {
-        // TODO Auto-generated constructor stub
+       
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+    /*Recebe os dados por url paramenters */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	/*Recebe os dados enviados por forms */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		System.out.println(request.getParameter("nome"));
+		System.out.println(request.getParameter("idade"));
 	}
 
 }
